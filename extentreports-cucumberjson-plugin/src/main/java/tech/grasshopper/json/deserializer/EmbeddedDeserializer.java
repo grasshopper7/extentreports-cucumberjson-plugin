fb1d69fcc0b97c8b2ddeef6cf35895f3e6650087
grasshopper7 <grass.hopper.moc@gmail.com>
2021-01-27 18:15:27 +0530

    Fix for NPE in EmbeddedProcessor

--- extentreports-cucumberjson-plugin/src/main/java/tech/grasshopper/json/deserializer/EmbeddedDeserializer.java
+++ extentreports-cucumberjson-plugin/src/main/java/tech/grasshopper/json/deserializer/EmbeddedDeserializer.java
@@ -11,22 +11,25 @@ import tech.grasshopper.pojo.Embedded;
 import tech.grasshopper.processor.EmbeddedProcessor;
 
 public class EmbeddedDeserializer implements JsonDeserializer<Embedded> {
-	
+
 	private EmbeddedProcessor embeddedProcessor;
-	
+
 	public EmbeddedDeserializer(EmbeddedProcessor embeddedProcessor) {
 		this.embeddedProcessor = embeddedProcessor;
 	}
 
 	@Override
-    public Embedded deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) {
-		JsonObject jsonObject = json.getAsJsonObject();        
-        Embedded embedded = new Embedded();
-        embedded.setData(jsonObject.get("data").getAsString());
-        embedded.setMimeType(jsonObject.get("mime_type").getAsString());
-        embedded.setName(jsonObject.get("name").getAsString());
-                
-        embeddedProcessor.processEmbedding(embedded);        		
+	public Embedded deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) {
+		JsonObject jsonObject = json.getAsJsonObject();
+		Embedded embedded = new Embedded();
+		embedded.setData(jsonObject.get("data").getAsString());
+		embedded.setMimeType(jsonObject.get("mime_type").getAsString());
+		if (jsonObject.has("name"))
+			embedded.setName(jsonObject.get("name").getAsString());
+		else
+			embedded.setName("");
+
+		embeddedProcessor.processEmbedding(embedded);
 		return embedded;
 	}
 }
